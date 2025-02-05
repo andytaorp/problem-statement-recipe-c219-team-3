52d871e5843bae5fcb278b2131a781e0b69e32db
Carrie <23023478@myrp.edu.sg>
2025-02-05 10:29:20 +0800

    2nd change name

---
+++ frontend/src/components/RecipeDetails.js
@@ -0,0 +1,40 @@
+import { useRecipesContext } from '../hooks/useRecipesContext'
+import { useAuthContext } from '../hooks/useAuthContext'
+
+// date fns
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
+const RecipeDetails = ({ recipe }) => {
+  const { dispatch } = useRecipesContext()
+  const { user } = useAuthContext()
+
+  const handleClick = async () => {
+    if (!user) {
+      return
+    }
+
+    const response = await fetch(`${process.env.REACT_APP_API_URL}api/recipes/${recipe._id}` , {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${user.token}`
+      }
+    })
+    const json = await response.json()
+
+    if (response.ok) {
+      dispatch({type: 'DELETE_RECIPES', payload: json})
+    }
+  }
+
+  return (
+    <div className="recipes-details">
+      <h4>{recipe.title}</h4>
+      <p><strong>Load (kg): </strong>{recipe.load}</p>
+      <p><strong>Reps: </strong>{recipe.reps}</p>
+      <p>{formatDistanceToNow(new Date(recipe.createdAt), { addSuffix: true })}</p>
+      <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
+    </div>
+  )
+}
+
+export default RecipeDetails
